edef7cec01800bcda1a15791752fca8a46728eb3
TOXIC <thrax989@yahoo.com>
2020-05-01 22:14:43 -0700

    Discord Broadcasting Functionality For Lua Scripts TEST

--- MMOCoreORB/src/server/zone/objects/player/LuaPlayerObject.cpp
+++ MMOCoreORB/src/server/zone/objects/player/LuaPlayerObject.cpp
@@ -104,6 +104,8 @@ Luna<LuaPlayerObject>::RegType LuaPlayerObject::Register[] = {
 		{ "hasGcwTef", &LuaPlayerObject::hasGcwTef },
 		{ "getPvpRating", &LuaPlayerObject::getPvpRating },
 		{ "getFirstname", &LuaPlayerObject::getFirstname },
+		{ "broadcastToServer", &LuaPlayerObject::broadcastToServer },
+		{ "broadcastToDiscord", &LuaPlayerObject::broadcastToDiscord },
 		{ 0, 0 }
 };
 
@@ -954,3 +956,17 @@ int LuaPlayerObject::getFirstname(lua_State* L) {
 
 	return 1;
 }
+
+int LuaPlayerObject::broadcastToServer(lua_State* L) {
+	String message = lua_tostring(L, -1);
+	ZoneServer* zServ = realObject->getZoneServer();
+	zServ->getChatManager()->broadcastGalaxy(nullptr, message);
+	return 1;
+}
+
+int LuaPlayerObject::broadcastToDiscord(lua_State* L) {
+	String message = lua_tostring(L, -1);
+	ZoneServer* zServ = realObject->getZoneServer();
+	zServ->getChatManager()->handleGeneralDiscordChat(nullptr, message);
+	return 1;
+} 
