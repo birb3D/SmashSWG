19799719464bf1edfa305eb4c3fe51b2cc097181
Smashley <ashley66444@yahoo.com>
2020-11-15 02:32:22 -0500

    Starter Medic Crafting XP fixes

--- MMOCoreORB/bin/scripts/object/draft_schematic/chemistry/medpack_wound_action_a.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/chemistry/medpack_wound_action_a.lua
@@ -54,7 +54,7 @@ object_draft_schematic_chemistry_medpack_wound_action_a = object_draft_schematic
 	factoryCrateType = "object/factory/factory_crate_chemicals.iff",
 
 	xpType = "crafting_medicine_general",
-	xp = 30,
+	xp = 42,
 
 	assemblySkill = "medicine_assembly",
 	experimentingSkill = "medicine_experimentation",
