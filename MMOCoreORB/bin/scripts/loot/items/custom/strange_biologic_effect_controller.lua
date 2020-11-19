df667c874fe3a0ddbd5d138dc0687f2e38f39e19
Smashley <ashley66444@yahoo.com>
2020-11-19 09:29:39 -0500

    Testing new Strange Components

---
+++ MMOCoreORB/bin/scripts/loot/items/custom/strange_biologic_effect_controller.lua
@@ -0,0 +1,16 @@
+strange_biologic_effect_controller = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/component/chemistry/biologic_effect_controller_advanced.iff",
+	craftingValues = {
+		{"power", 30,60,0},
+		{"charges", -50,-50,0},
+		{"useCount",1,2,0}
+	},
+	customizationStringName = {},
+	customizationValues = {}
+}
+
+
+addLootItemTemplate("strange_biologic_effect_controller", strange_biologic_effect_controller)
