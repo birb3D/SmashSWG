cfec95069a13b47cb6785fe9a5f7d51cf25437a2
Smashley <ashley66444@yahoo.com>
2020-11-24 12:58:02 -0500

    Looted Component Quick Update

--- MMOCoreORB/bin/scripts/loot/items/custom/strange_biologic_effect_controller.lua
+++ MMOCoreORB/bin/scripts/loot/items/custom/strange_biologic_effect_controller.lua
@@ -1,12 +1,12 @@
 strange_biologic_effect_controller = {
 	minimumLevel = 0,
 	maximumLevel = -1,
-	customObjectName = "",
+	customObjectName = "Strange Advanced Biological Effect Controller",
 	directObjectTemplate = "object/tangible/component/chemistry/biologic_effect_controller_advanced.iff",
 	craftingValues = {
-		{"power", 60,140,0},
+		{"power", 60,160,0},
 		{"charges", -100,-100,0},
-		{"useCount",1,2,0}
+		{"useCount",1,8,0}
 	},
 	customizationStringName = {},
 	customizationValues = {}
