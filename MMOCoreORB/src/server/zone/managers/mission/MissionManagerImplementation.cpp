d4514b27b9b3fa0ab87357baed151dfdd6f7cec2
Smashley <ashley66444@yahoo.com>
2020-12-20 17:21:29 -0500

    Wider selection of missions
    Change-Id: I5e24f36ba707bb649ffd4903544d1dc2e0eb48ff

--- MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/mission/MissionManagerImplementation.cpp
@@ -1735,7 +1735,7 @@ LairSpawn* MissionManagerImplementation::getRandomLairSpawn(CreatureObject* play
 		return nullptr;
 
 	const Vector<Reference<LairSpawn*> >* availableLairList = nullptr;
-	int minLevelCeiling = 20;
+	int minLevelCeiling = 8;
 
 	if (type == MissionTypes::DESTROY) {
 		String missionGroup;
