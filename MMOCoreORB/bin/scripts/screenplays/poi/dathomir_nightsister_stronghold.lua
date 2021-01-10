b77dccaee8ac3cde029d071077bdadcb0976942e
Omegaice <omegaice@gmail.com>
2021-01-10 14:26:50 -0500

    Updated nightsister queen spawn to be a little random

--- MMOCoreORB/bin/scripts/screenplays/poi/dathomir_nightsister_stronghold.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/dathomir_nightsister_stronghold.lua
@@ -278,7 +278,7 @@ function NightSisterStrongholdScreenPlay:respawnAxkvaMin()
 end
 
 function NightSisterStrongholdScreenPlay:axkvaKilled(pAxkvaMin)
-	createEvent(86400 * 1000, "NightSisterStrongholdScreenPlay", "respawnAxkvaMin", nil, "")
+	createEvent((64800 + getRandomNumber(21600)) * 1000, "NightSisterStrongholdScreenPlay", "respawnAxkvaMin", nil, "")
 
 	return 1
 end
