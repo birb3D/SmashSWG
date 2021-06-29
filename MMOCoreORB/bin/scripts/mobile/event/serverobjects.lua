f25dd5a99954e64e905454fd9581b1c23408d254
christian <thrax989@yahoo.com>
2021-06-28 23:32:45 -0700

    Big Game Hunters Testing

--- MMOCoreORB/bin/scripts/mobile/event/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/event/serverobjects.lua
@@ -5,6 +5,15 @@ includeFile("event/life_day/oraalarri.lua")
 includeFile("event/life_day/radrrl.lua")
 includeFile("event/life_day/tebeurra.lua")
 
+--World Boss
+includeFile("event/custom/big_game_hunter.lua")
+includeFile("event/world_boss/deathsting_boss.lua")
+includeFile("event/world_boss/acklay_boss.lua")
+includeFile("event/world_boss/rancor_boss.lua")
+includeFile("event/world_boss/wampa_boss.lua")
+includeFile("event/world_boss/geonosian_boss.lua")
+
+
 --CityControlLanding
 includeFile("event/city_control_landing/darth_vader_city_control.lua")
 includeFile("event/city_control_landing/leia_organa_city_control.lua")
