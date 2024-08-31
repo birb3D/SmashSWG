6733e17862049ec332b6c73fb71ea11617c2a6b5
InfiniteAxis <InfiniteAxis@users.noreply.github.com>
2024-08-31 16:55:49 -0400

    Add Loot Tables for Large Resource Containers, and Massive Resource Containers

---
+++ MMOCoreORB/bin/scripts/loot/groups/large_resource/large_resource_all.lua
@@ -0,0 +1,16 @@
+large_resource_all = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{groupTemplate = "large_resource_chemical", weight = 1428571},
+		{groupTemplate = "large_resource_flora", weight = 1428571},
+		{groupTemplate = "large_resource_gemstone", weight = 1428571},
+		{groupTemplate = "large_resource_metal", weight = 1428571},
+		{groupTemplate = "large_resource_ore", weight = 1428571},
+		{groupTemplate = "large_resource_water", weight = 1428571}, 
+		{groupTemplate = "large_resource_creature", weight = 1428571},
+	}
+}
+
+addLootGroupTemplate("large_resource_all", large_resource_all)
