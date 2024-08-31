3555e55bd097064c2c5ff16a252a697d4afce02a
InfiniteAxis <InfiniteAxis@users.noreply.github.com>
2024-08-31 17:51:59 -0400

    Fix weights, and remove extras from Legendary Strange Items Table

--- MMOCoreORB/bin/scripts/loot/groups/legendary_strange_items.lua
+++ MMOCoreORB/bin/scripts/loot/groups/legendary_strange_items.lua
@@ -3,50 +3,33 @@ legendary_strange_items = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
+		{itemTemplate = "legendary_strange_biologic_effect_controller", weight = 760000}, -- Weighted more heavily because only one for this class
+		{itemTemplate = "legendary_strange_armor_segment_bone", weight = 380000},
+		{itemTemplate = "legendary_strange_armor_segment_zam", weight = 380000},
+		{itemTemplate = "legendary_strange_armor_segment_chitin", weight = 380000},
+		{itemTemplate = "legendary_strange_armor_segment_padded", weight = 380000},
+		{itemTemplate = "legendary_strange_armor_segment_ubese", weight = 380000},
+		{itemTemplate = "legendary_strange_armor_segment_tantel", weight = 380000},
+		{itemTemplate = "legendary_strange_bio_component_food_light", weight = 380000},
+		{itemTemplate = "legendary_strange_bio_component_food_medium", weight = 380000},
+		{itemTemplate = "legendary_strange_bio_component_food_heavy", weight = 380000},
+		{itemTemplate = "legendary_strange_scope_weapon_advanced", weight = 380000},
+		{itemTemplate = "legendary_strange_sword_core_advanced", weight = 380000},
+		{itemTemplate = "legendary_strange_reinforcement_core_advanced", weight = 380000},
+		{itemTemplate = "legendary_strange_vibro_unit_advanced", weight = 380000},
+		{itemTemplate = "legendary_strange_stock_advanced", weight = 380000},
+		{itemTemplate = "legendary_strange_droid_combat_module_slow", weight = 380000},
+		{itemTemplate = "legendary_strange_droid_combat_module_fast", weight = 380000},
+		{itemTemplate = "legendary_strange_structure_fluidic_drilling_pumping_unit", weight = 380000},
+		{itemTemplate = "legendary_strange_structure_harvesting_mechanism", weight = 380000},
+		{itemTemplate = "legendary_strange_structure_heavy_harvesting_mechanism", weight = 380000},
+		{itemTemplate = "legendary_strange_structure_light_ore_mining_unit", weight = 380000},
+		{itemTemplate = "legendary_strange_structure_ore_mining_unit", weight = 380000},
+		{itemTemplate = "legendary_strange_structure_turbo_fluidic_drilling_pumping_unit", weight = 380000},
 
-		{groupTemplate = "resource_large_chemical", weight = 142857},
-		{groupTemplate = "resource_large_flora", weight = 142857},
-		{groupTemplate = "resource_large_gemstone", weight = 142857},
-		{groupTemplate = "resource_large_metal", weight = 142857},
-		{groupTemplate = "resource_large_ore", weight = 142857},
-		{groupTemplate = "resource_large_water", weight = 142857}, 
-		{groupTemplate = "resource_large_creature", weight = 142857},
-
-
-		-- 15.95%
-		{groupTemplate = "looted_container", weight = 1595001}, 
-
-		-- 10%
-		{groupTemplate = "color_crystals", weight = 1000000}, 
-
-		-- 64%
-		{itemTemplate = "legendary_strange_biologic_effect_controller", weight = 680000}, -- Weighted more heavily because only one for this class
-		{itemTemplate = "legendary_strange_armor_segment_bone", weight = 260000},
-		{itemTemplate = "legendary_strange_armor_segment_zam", weight = 260000},
-		{itemTemplate = "legendary_strange_armor_segment_chitin", weight = 260000},
-		{itemTemplate = "legendary_strange_armor_segment_padded", weight = 260000},
-		{itemTemplate = "legendary_strange_armor_segment_ubese", weight = 260000},
-		{itemTemplate = "legendary_strange_armor_segment_tantel", weight = 260000},
-		{itemTemplate = "legendary_strange_bio_component_food_light", weight = 260000},
-		{itemTemplate = "legendary_strange_bio_component_food_medium", weight = 260000},
-		{itemTemplate = "legendary_strange_bio_component_food_heavy", weight = 260000},
-		{itemTemplate = "legendary_strange_scope_weapon_advanced", weight = 260000},
-		{itemTemplate = "legendary_strange_sword_core_advanced", weight = 260000},
-		{itemTemplate = "legendary_strange_reinforcement_core_advanced", weight = 260000},
-		{itemTemplate = "legendary_strange_vibro_unit_advanced", weight = 260000},
-		{itemTemplate = "legendary_strange_stock_advanced", weight = 260000},
-		{itemTemplate = "legendary_strange_droid_combat_module_slow", weight = 260000},
-		{itemTemplate = "legendary_strange_droid_combat_module_fast", weight = 260000},
-		{itemTemplate = "legendary_strange_structure_fluidic_drilling_pumping_unit", weight = 260000},
-		{itemTemplate = "legendary_strange_structure_harvesting_mechanism", weight = 260000},
-		{itemTemplate = "legendary_strange_structure_heavy_harvesting_mechanism", weight = 260000},
-		{itemTemplate = "legendary_strange_structure_light_ore_mining_unit", weight = 260000},
-		{itemTemplate = "legendary_strange_structure_ore_mining_unit", weight = 260000},
-		{itemTemplate = "legendary_strange_structure_turbo_fluidic_drilling_pumping_unit", weight = 260000},
-
-		-- 0.05%
-		{itemTemplate = "speederbike_swoop_deed", weight = 5000},
+		-- 5%
+		{itemTemplate = "speederbike_swoop_deed", weight = 500000},
 	}
 }
 
-addLootGroupTemplate("strange_items", strange_items)
+addLootGroupTemplate("legendary_strange_items", legendary_strange_items)
