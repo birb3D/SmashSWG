d6b97911a00649b147484338ee24b616610e6159
VinnyVega69 <vinnyvegagaming@gmail.com>
2024-08-31 17:32:32 -0400

    Saved resource_all folder

---
+++ MMOCoreORB/bin/scripts/loot/groups/resource/resource_all.lua
@@ -0,0 +1,16 @@
+resource_all = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{groupTemplate = "resource_chemical", weight = 1428571},
+		{groupTemplate = "resource_flora", weight = 1428571},
+		{groupTemplate = "resource_gemstone", weight = 1428571},
+		{groupTemplate = "resource_metal", weight = 1428571},
+		{groupTemplate = "resource_ore", weight = 1428571},
+		{groupTemplate = "resource_water", weight = 1428571}, 
+		{groupTemplate = "resource_creature", weight = 1428571},
+	}
+}
+
+addLootGroupTemplate("resource_all", resource_all)
