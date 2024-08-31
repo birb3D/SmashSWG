348a4c0898158dc4a499ea09db0b056d4262c2f1
InfiniteAxis <InfiniteAxis@users.noreply.github.com>
2024-08-31 17:38:04 -0400

    Add Legendary variants for Strange Items

---
+++ MMOCoreORB/bin/scripts/loot/items/custom/legendary_strange_biologic_effect_controller.lua
@@ -0,0 +1,16 @@
+legendary_strange_biologic_effect_controller = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Legendary Strange Advanced Biological Effect Controller",
+	directObjectTemplate = "object/tangible/component/chemistry/biologic_effect_controller_advanced.iff",
+	craftingValues = {
+		{"power", 400,560,0},
+		{"charges", -100,0,0},
+		{"useCount",1,8,0}
+	},
+	customizationStringName = {},
+	customizationValues = {}
+}
+
+
+addLootItemTemplate("legendary_strange_biologic_effect_controller", legendary_strange_biologic_effect_controller)
